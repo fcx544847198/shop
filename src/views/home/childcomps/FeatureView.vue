5893a1539e6300915e01b23a38d7b14688c6df5d
fcx544847198 <544847198@qq.com>
2019-09-03 16:11:15 +0800

    1

--- src/views/home/childcomps/FeatureView.vue
+++ src/views/home/childcomps/FeatureView.vue
@@ -1,18 +1,19 @@
 <template>
-    <div>
-        <img src="../../../assets/img/home/recommend_bg.jpg" alt="">
-    </div>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="../../../assets/img/home/recommend_bg.jpg" alt />
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'FeatureView'
-}
+  name: "FeatureView"
+};
 </script>
 
 <style scoped>
-    img{
-        width: 300px;
-        height: 300px;
-    }
+.feature img {
+  width: 100%;
+}
 </style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -6,6 +6,29 @@
     <homeswiper :banners='banners'></homeswiper>
     <recommends-view :recommends="recommends"></recommends-view>
     <feature-view></feature-view>
+
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+    </ul>
   </div>
 </template>
 
@@ -41,8 +64,16 @@ export default {
 </script>
 
 <style scope>
+#home{
+ padding-top: 44px; 
+}
 .home-nav {
   background-color:var(--color-tint);
   color:white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
 }
 </style>
